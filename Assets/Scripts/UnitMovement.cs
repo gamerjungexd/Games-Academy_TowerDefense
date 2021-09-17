8c1ae7f0c055e09fe1239fc9daba273e96329941
Onii-ChanFL <gamerjunge@gmail.com>
2021-09-17 10:31:08 +0200

    Implemented UnitMovement

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class UnitMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints = null;
+
+    [SerializeField] private float toleranceDistance = 0.1f;
+
+    private int waypointIndex = 0;
+    private Vector3 nextPosition;
+    private CharacterController characterController = null;
+
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        nextPosition = waypoints[0].position;
+    }
+
+    void FixedUpdate()
+    {
+        if (((Vector2)nextPosition - (Vector2)transform.position).magnitude <= toleranceDistance)
+        {
+            waypointIndex++;
+            nextPosition = waypoints[waypointIndex].position;
+        }
+
+        Vector2 direction = ((Vector2)nextPosition - (Vector2)transform.position).normalized * Time.fixedDeltaTime;
+        characterController.Move(direction);
+    }
+
+}
