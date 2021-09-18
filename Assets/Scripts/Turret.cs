1c8a8be15479fc173ddb4ce59fe801a27f551dfb
Onii-ChanFL <gamerjunge@gmail.com>
2021-09-18 09:47:01 +0200

    Implemented AttackAnimation

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -6,11 +6,19 @@ public class Turret : MonoBehaviour
 {
     [SerializeField] private Transform modelHead = null;
 
+    [SerializeField] private int damage = 2;
+    [SerializeField] private float attackSpeed = 1f;
+
+    [SerializeField] private float effectTime = 0.5f;
+    [SerializeField] private GameObject[] attackEffect = null;
+
+    private int indexAttackEffect = 0;
     private List<GameObject> targets = new List<GameObject>();
+
 
     void Start()
     {
-
+        StartCoroutine(ShotTarget());
     }
 
     void Update()
@@ -20,6 +28,44 @@ public class Turret : MonoBehaviour
             modelHead.rotation = Quaternion.Euler(0f, 0f, Vector3.SignedAngle(Vector3.up, (targets[0].transform.position - modelHead.position), Vector3.forward));
         }
     }
+
+    public IEnumerator ShotTarget()
+    {
+        yield return new WaitForSeconds(attackSpeed);
+
+        yield return new WaitUntil(() => targets.Count > 0);
+
+        OnAttack();
+
+        StartCoroutine(ShotTarget());
+    }
+
+    public void OnAttack()
+    {
+        StartCoroutine(ShowAttack());
+    }
+
+    private IEnumerator ShowAttack()
+    {
+        if(attackEffect.Length <= 0)
+        {
+            yield break;
+        }
+
+        attackEffect[indexAttackEffect].SetActive(true);
+        yield return new WaitForSeconds(effectTime);
+        attackEffect[indexAttackEffect].SetActive(false);
+
+        if (indexAttackEffect < attackEffect.Length - 1)
+        {
+            indexAttackEffect++;
+        }
+        else if (attackEffect.Length > 1 && indexAttackEffect >= attackEffect.Length - 1)
+        {
+            indexAttackEffect = 0;
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
